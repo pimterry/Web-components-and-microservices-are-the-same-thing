d2152e973c967ddc35c9f3b39acb00eae4f8aa6a
pimterry <pimterry@gmail.com>
2014-09-27 18:26:43 +0100

    Add login endpoint to auth

--- comparably/auth/app.js
+++ comparably/auth/app.js
@@ -1,10 +1,26 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
+app.use(bodyParser.text());
 
-app.get('/tokens/validate/:token', function (req, res) {
+app.get('/validate/token/:token', function (req, res) {
     var token = req.params.token;
     res.json({ token: token, valid: token === "1" });
+});
+
+app.post('/user/:username/login', function (req, res) {
+    var username = req.params.username;
+    var password = req.body;
+
+    var success = (username === "tim" && password === "password1");
+
+    if (success) {
+        var token = 1;
+        res.json({ username: username, token: token });
+    } else {
+        res.status(401).end();
+    }
 });
 
 var port = process.env.PORT || 8085;
