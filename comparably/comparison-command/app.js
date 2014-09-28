0d968fbf15a42bd8de5c47db591f8cc269e3bbdd
pimterry <pimterry@gmail.com>
2014-09-28 22:12:31 +0100

    Add insert/update endpoints for items too

--- comparably/comparison-command/app.js
+++ comparably/comparison-command/app.js
@@ -11,6 +11,45 @@ app.use(cors());
 var db = new neo4j.GraphDatabase({ url: process.env.NEO4J_URL });
 var authServer = "http://comparably-auth.herokuapp.com";
 
+// Creates a new item with the name from the JSON body, returning the id
+// Unauthenticated, since items are not inherently tied to comparisons, and
+// we only consider permissions at comparison level. Definitely just demo code!
+app.post('/items', function (req, res) {
+    var data = req.body;
+
+    var insertQuery = "create (i:Item { name: {name} }) return id(i) as itemId";
+    var parameters = { name: data.name };
+
+    db.query(insertQuery, parameters, function (err, results) {
+        if (err) {
+            console.error(err);
+            res.status(500).end();
+        } else {
+            res.json({ success: true, id: results[0].itemId });
+        }
+    });
+});
+
+// Updates an item with a new name from the JSON body
+// Unauthenticated, since items are not inherently tied to comparisons, and
+// we only consider permissions at comparison level. Definitely just demo code!
+app.put('/items/:itemId', function (req, res) {
+    var itemId = parseInt(req.params.itemId, 10);
+    var data = req.body;
+
+    var updateQuery = "match (i:Item) where id(i) = {itemId} set i.name = {name}";
+    var parameters = { name: data.name, itemId: itemId };
+
+    db.query(updateQuery, parameters, function (err, results) {
+        if (err) {
+            console.error(err);
+            res.status(500).end();
+        } else {
+            res.json({ success: true, });
+        }
+    });
+});
+
 // Creates a new facet of a comparison describing the given item,
 // with the score/description from the JSON body
 app.post('/comparisons/:comparisonId/items/:itemId', function (req, res) {
